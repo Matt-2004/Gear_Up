8b4f8f3ab0889cda8f3e714edecb254b0ee1b86a
Cap <wai450013956@gmail.com>
2026-02-22 21:25:32 +0700

    Fix bug!

--- gear-up/src/app/profile/dealer/cars/add/AddNewCarContext.tsx
+++ gear-up/src/app/profile/dealer/cars/add/AddNewCarContext.tsx
@@ -1,72 +1,74 @@
-"use client";
+"use client"
 
-import { CarItems } from "@/app/types/car.types";
+import { CarItems } from "@/app/types/car.types"
 import {
-  createContext,
-  ReactNode,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+	createContext,
+	ReactNode,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from "react"
 
 type SubmitVehicle = Omit<CarItems, "carImages" | "id"> & {
-  carImages: File[];
-};
+	carImages: File[]
+}
 
 interface IVehicleContextType {
-  addedCar: SubmitVehicle | undefined;
-  updateAddedCar: (data: Partial<SubmitVehicle>) => void;
+	addedCar: SubmitVehicle | undefined
+	updateAddedCar: (data: Partial<SubmitVehicle>) => void
 }
 
 export const VehicleContext = createContext<IVehicleContextType | undefined>(
-  undefined,
-);
+	undefined,
+)
 
 export default function VehicleContextProvider({
-  children,
+	children,
 }: {
-  children: ReactNode;
+	children: ReactNode
 }) {
-  const [vehicleData, setVehicleData] = useState<SubmitVehicle>({
-    title: "",
-    description: "",
-    model: "",
-    make: "",
-    year: 0,
-    price: 0,
-    color: "",
-    mileage: 0,
-    seatingCapacity: 0,
-    engineCapacity: 0,
-    carImages: [],
-    fuelType: "Default",
-    carCondition: "Default",
-    transmissionType: "Default",
-    carStatus: "",
-    carValidationStatus: "",
-    vin: "",
-    licensePlate: "",
-  });
+	const [vehicleData, setVehicleData] = useState<SubmitVehicle>({
+		title: "",
+		description: "",
+		model: "",
+		make: "",
+		year: 0,
+		price: 0,
+		color: "",
+		mileage: 0,
+		seatingCapacity: 0,
+		engineCapacity: 0,
+		carImages: [],
+		fuelType: "Default",
+		carCondition: "Default",
+		transmissionType: "Default",
+		carStatus: "",
+		carValidationStatus: "",
+		vin: "",
+		licensePlate: "",
+		name: undefined,
+		dealerId: "", // Add a default value for dealerId
+	})
 
-  useEffect(() => {
-    console.log("Car data in context:: ", vehicleData);
-  }, [vehicleData]);
+	useEffect(() => {
+		console.log("Car data in context:: ", vehicleData)
+	}, [vehicleData])
 
-  const updateAddedCar = useCallback((data: Partial<SubmitVehicle>) => {
-    setVehicleData((prev) => ({ ...prev, ...data }));
-  }, []);
-  return (
-    <VehicleContext.Provider value={{ addedCar: vehicleData, updateAddedCar }}>
-      {children}
-    </VehicleContext.Provider>
-  );
+	const updateAddedCar = useCallback((data: Partial<SubmitVehicle>) => {
+		setVehicleData((prev) => ({ ...prev, ...data }))
+	}, [])
+	return (
+		<VehicleContext.Provider value={{ addedCar: vehicleData, updateAddedCar }}>
+			{children}
+		</VehicleContext.Provider>
+	)
 }
 
 export function useVehicleContext() {
-  const context = useContext(VehicleContext);
-  if (!context) {
-    throw new Error("useVehicleContext must be used inside a Provider");
-  }
-  return context;
+	const context = useContext(VehicleContext)
+	if (!context) {
+		throw new Error("useVehicleContext must be used inside a Provider")
+	}
+	return context
 }
